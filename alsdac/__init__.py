e9f14918a930a4ee1ff43eab7d8171232db9f345
Ron Pandolfi <ronpandolfi@gmail.com>
2017-12-15 17:04:24 -0800

    Fix deprecated usage; Add many commands

--- alsdac/__init__.py
+++ alsdac/__init__.py
@@ -2,16 +2,23 @@
 
 import sys
 import trio
-from typing import Union, Tuple
+from typing import Union, Tuple, List
 
 # arbitrary, but:
 # - must be in between 1024 and 65535
 # - can't be in use by some other program on your computer
 # - must match what we set in our echo server
-PORT = 12345
+PORT = 55000
 # How much memory to spend (at most) on each call to recv. Pretty arbitrary,
 # but shouldn't be too big or too small.
 BUFSIZE = 16384
+
+ENCODING = 'ascii'
+
+SERVER_ADDRESS = "131.243.81.35"  # Dula's sample stage server
+
+
+# SERVER_ADDRESS = "131.243.81.43" # Dula's primary BCS server
 
 
 def get(data):
@@ -19,46 +26,68 @@ def get(data):
         result = None
 
         async def sender(client_sock, data):
-            print("sender: started!")
-            print("sender: sending {!r}".format(data))
-            await client_sock.sendall(bytes(data, 'utf-8'))
+            # print("sender: started!")
+            # print("sender: sending {!r}".format(data))
+            await client_sock.send_all(bytes(data, ENCODING))
 
         async def receiver(client_sock):
-            print("receiver: started!")
-            _data = await client_sock.recv(BUFSIZE)
-            print("receiver: got data {!r}".format(_data))
+            # print("receiver: started!")
+            _data = await client_sock.receive_some(BUFSIZE)
+            # print("receiver: got data {!r}".format(_data))
             if not data:
-                print("receiver: connection closed")
+                # print("receiver: connection closed")
                 sys.exit()
             nonlocal result
             result = _data
 
-        print("parent: connecting to 127.0.0.1:{}".format(PORT))
+        # print("parent: connecting to 127.0.0.1:{}".format(PORT))
         with trio.socket.socket() as client_sock:
-            await client_sock.connect(("127.0.0.1", PORT))
+            await client_sock.connect((SERVER_ADDRESS, PORT))
+            client_sock = trio.SocketStream(client_sock)
             async with trio.open_nursery() as nursery:
-                print("parent: spawning sender...")
+                # print("parent: spawning sender...")
                 nursery.start_soon(sender, client_sock, data)
 
-                print("parent: spawning receiver...")
+                # print("parent: spawning receiver...")
                 nursery.start_soon(receiver, client_sock)
 
         return result
+
     return trio.run(_get, data)
 
 
-def GetMotorPos(motorname:str) -> float:
-    return float(get(f'GetMotorPos({motorname})'))
+def GetMotorPos(motorname: str) -> float:
+    return float(get(f'GetMotorPos({motorname})\r\n'))
 
 
-def MoveMotor(motorname:str, pos:Union[float, int]) -> bool:
-    return bool(get(f'MoveMotor({motorname}, {pos})'))
+def MoveMotor(motorname: str, pos: Union[float, int]) -> bool:
+    return bool(get(f'MoveMotor({motorname}, {pos})\r\n'))
 
-def GetSoftLimits(motorname:str) -> Tuple[float, float]:
-    return tuple(map(float,get(f'GetSoftLimits({motorname})').split(b' ')))[:2] # The 2 just makes pylint happy :)
 
-if __name__=='__main__':
-    GetMotorPos('test')
-    MoveMotor('test', 1)
-    GetMotorPos('test')
-    MoveMotor('test', 0)
+def ListAIs()->List[str]:
+    return str(get(f'ListAIs\r\n'), ENCODING).split('\r\n')
+
+
+def GetSoftLimits(motorname: str) -> Tuple[float, float]:
+    return tuple(map(float, get(f'GetSoftLimits({motorname})\r\n').split(b' ')))[:2]  # The 2 just makes pylint happy :)
+
+
+def StartAcquire(time: float, counts: int):
+    return bool(get(f'StartAcquire({time},{counts}\r\n'))
+
+def GetMotorStatus(motorname: str) -> bool: # returns true if move complete
+    return get(f'GetMotorStat({motorname})\r\n').startswith(b'Move finished')
+    # TODO: also return the datetime as second value
+
+def ListDIOs()->List[str]:
+    return str(get(f'ListDIOs\r\n'), ENCODING).split('\r\n')
+
+def GetFreerun(ainame)->float:
+    return float(get(f'GetFreerun({ainame})\r\n'))
+
+if __name__ == '__main__':
+    GetMotorPos('Loading Stage (um)')
+    MoveMotor('Loading Stage (um)', 1)
+    GetMotorPos('Loading Stage (um)')
+    MoveMotor('Loading Stage (um)', 0)
+    GetMotorPos('Loading Stage (um)')
