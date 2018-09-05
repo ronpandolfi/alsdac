ef70e511990fffe7646df474dc21702e6eb7603f
Ronald Pandolfi <ronpandolfi@gmail.com>
2018-09-05 04:55:43 -0700

    add analog input group; finish ophyd epicsmotor compat

--- alsdac/caproto/__init__.py
+++ alsdac/caproto/__init__.py
@@ -6,25 +6,39 @@ import functools
 import alsdac
 
 
-class AnalogInput(PVGroup):
-    value = pvproperty(value=[0],
-                     dtype=float,
-                     read_only=True)
+def AnalogInput(name):
+    class AnalogInput(PVGroup):
+        value = pvproperty(value=[0],
+                         dtype=float,
+                         read_only=True)
 
-    @value.getter
-    async def value(self, instance):
-        # name = instance.pvspec.attr
-        return alsdac.GetFreerun('Beam Current')
+        @value.getter
+        async def value(self, instance):
+            return alsdac.GetFreerun(name)
+
+    return AnalogInput
 
 
 def Motor(name):
     class Motor(PVGroup):
-        VAL = pvproperty(value=[0],
-                         dtype=float,
-                         )
-        RBV = pvproperty(value=[0],
-                         dtype=float,
-                         )
+        # FIXME: most of these don't actually do anything
+        OFF = pvproperty(value=[0], dtype=float)
+        DIR = pvproperty(value=[0], dtype=float)
+        FOFF = pvproperty(value=[0], dtype=float)
+        SET = pvproperty(value=[0], dtype=float)
+        VELO = pvproperty(value=[0], dtype=float)
+        ACCL = pvproperty(value=[0], dtype=float)
+        MOVN = pvproperty(value=[0], dtype=float)
+        DMOV = pvproperty(value=[0], dtype=float)
+        HLS = pvproperty(value=[0], dtype=float)
+        LLS = pvproperty(value=[0], dtype=float)
+        TDIR = pvproperty(value=[0], dtype=float)
+        STOP = pvproperty(value=[0], dtype=float)
+        HOMF = pvproperty(value=[0], dtype=float)
+        HOMR = pvproperty(value=[0], dtype=float)
+        EGU = pvproperty(value=[0], dtype=float)
+        VAL = pvproperty(value=[0], dtype=float)
+        RBV = pvproperty(value=[0], dtype=float)
         @VAL.putter
         async def VAL(self, instance, value):
             print(f'Setting {instance} to {value}')
@@ -32,7 +46,7 @@ def Motor(name):
 
             # TODO: LABVIEW TCP interface has no command to get the setpoint; request this addition; fill in getter
 
-        # # And the readback getter:
+        # # TODO: And the readback getter
         # @VAL.getter
         # async def VAL(obj, instance):
         #     # NOTE: this is effectively a no-operation method
@@ -46,6 +60,12 @@ def Motor(name):
     return Motor
 
 class Beamline(PVGroup):
+    @SubGroup(prefix='ais:')
+    class AnalogInputs(PVGroup):
+        names = alsdac.ListAIs()
+        for name in names:
+            locals()[name] = SubGroup(AnalogInput(name), prefix=name+'.')
+
     @SubGroup(prefix='motors:')
     class Motors(PVGroup):
         names = alsdac.ListMotors()
@@ -64,4 +84,5 @@ if __name__ == '__main__':
     # import os
     # os.environ['OPHYD_CONTROL_LAYER']='caproto'
     # import ophyd
-    # a = ophyd.EpicsSignal('beamline:m1:RBV','beamline:m1:VAL')
+    # a = ophyd.EpicsSignal('beamline:motors:fake.RBV','beamline:m1:VAL')
+    # b = ophyd.EpicsMotor('beamline:motors:fake', name='fake')
