805a2f6f294c26dc40f16894a380d16b93711851
Ronald Pandolfi <ronpandolfi@gmail.com>
2018-09-06 01:42:25 -0700

    Add instrument and dios

--- alsdac/caproto/__init__.py
+++ alsdac/caproto/__init__.py
@@ -3,23 +3,44 @@ from caproto.server import (pvproperty, PVGroup, SubGroup,
                             ioc_arg_parser, run, records)
 
 import alsdac
+import numpy as np
+
+class LVGroup(PVGroup):
+    @property
+    def devicename(self)->str:
+        return self.prefix.split(':')[-1].split('.')[0]
 
 
-class Instrument(PVGroup):
-    @SubGroup(prefix='cam1.')
-    class Camera(PVGroup):
-        blah = pvproperty(value=[0], dtype=float)
+class Instrument(LVGroup):
+    trigger = pvproperty(value=[0], dtype=bool)
+    read = pvproperty(value=[0], dtype=float)
+
+    # Non-standard PVs
+    exposure_time = pvproperty(value=[1], dtype=float)
+    exposure_counts = pvproperty(value=[1], dtype=int)
+
+    @trigger.putter
+    async def trigger(self, instance, value):
+        alsdac.StartInstrumentAcquire(self.devicename, self.exposure_time)
+
+    @read.getter
+    async def read(self, instance):
+        return alsdac.GetInstrumentAcquired2D(self.devicename)
 
 
-class AnalogInput(PVGroup):
+class AnalogInput(LVGroup):  # AIFields
     value = pvproperty(value=[0], dtype=float, read_only=True)
 
     @value.getter
     async def value(self, instance):
-        return alsdac.GetFreerun(self.prefix[:-1])
+        return alsdac.GetFreerun(self.devicename)
 
 
-class Motor(PVGroup):
+class DigitalInputOutput(AnalogInput):  # DigitalFields
+    pass
+
+
+class Motor(LVGroup):  # MotorFields
     # FIXME: most of these don't actually do anything
     OFF = pvproperty(value=[0], dtype=float)
     DIR = pvproperty(value=[0], dtype=float)
@@ -38,10 +59,10 @@ class Motor(PVGroup):
     EGU = pvproperty(value=[0], dtype=float)
     VAL = pvproperty(value=[0], dtype=float)
     RBV = pvproperty(value=[0], dtype=float)
+
     @VAL.putter
     async def VAL(self, instance, value):
-        print(f'Setting {instance} to {value}')
-        # alsdac.MoveMotor(name, value)
+        alsdac.MoveMotor(self.devicename, value)
 
     # TODO: LABVIEW TCP interface has no command to get the setpoint; request this addition; fill in getter
 
@@ -55,28 +76,33 @@ class Motor(PVGroup):
 
     @RBV.getter
     async def RBV(self, instance):
-        return alsdac.GetMotorPos(self.prefix[:-1])
+        return alsdac.GetMotorPos(self.devicename)
 
 
 class Beamline(PVGroup):
-
     @SubGroup(prefix='instruments:')
     class Detectors(PVGroup):
         names = alsdac.ListInstruments()
         for name in names:
-            locals()[name] = SubGroup(Instrument, prefix=name+':')
+            locals()[name] = SubGroup(Instrument, prefix=name + ':')
 
     @SubGroup(prefix='ais:')
     class AnalogInputs(PVGroup):
         names = alsdac.ListAIs()
         for name in names:
-            locals()[name] = SubGroup(AnalogInput, prefix=name+'.')
+            locals()[name] = SubGroup(AnalogInput, prefix=name + '.')
+
+    @SubGroup(prefix='dios:')
+    class DigitalInputOutputs(PVGroup):
+        names = alsdac.ListDIOs()
+        for name in names:
+            locals()[name] = SubGroup(DigitalInputOutput, prefix=name + '.')
 
     @SubGroup(prefix='motors:')
     class Motors(PVGroup):
         names = alsdac.ListMotors()
         for name in names:
-            locals()[name] = SubGroup(Motor, prefix=name+'.')
+            locals()[name] = SubGroup(Motor, prefix=name + '.')
 
 
 if __name__ == '__main__':
