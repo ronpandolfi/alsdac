e19fcaff2a6a8cb55d217f9ce0c8061086a59ce7
Ronald Pandolfi <ronpandolfi@gmail.com>
2018-09-05 22:41:56 -0700

    Remove outer methods

--- alsdac/caproto/__init__.py
+++ alsdac/caproto/__init__.py
@@ -1,66 +1,62 @@
 #!/usr/bin/env python3
 from caproto.server import (pvproperty, PVGroup, SubGroup,
-                            ioc_arg_parser, run)
+                            ioc_arg_parser, run, records)
 
 import alsdac
 
-def Instrument(name):
-    class Instrument(PVGroup):
-        @SubGroup(prefix='cam1.')
-        class Camera(PVGroup):
-            blah = pvproperty(value=[0], dtype=float)
-    return Instrument
 
-def AnalogInput(name):
-    class AnalogInput(PVGroup):
-        value = pvproperty(value=[0], dtype=float, read_only=True)
-
-        @value.getter
-        async def value(self, instance):
-            return alsdac.GetFreerun(name)
-
-    return AnalogInput
+class Instrument(PVGroup):
+    @SubGroup(prefix='cam1.')
+    class Camera(PVGroup):
+        blah = pvproperty(value=[0], dtype=float)
 
 
-def Motor(name):
-    class Motor(PVGroup):
-        # FIXME: most of these don't actually do anything
-        OFF = pvproperty(value=[0], dtype=float)
-        DIR = pvproperty(value=[0], dtype=float)
-        FOFF = pvproperty(value=[0], dtype=float)
-        SET = pvproperty(value=[0], dtype=float)
-        VELO = pvproperty(value=[0], dtype=float)
-        ACCL = pvproperty(value=[0], dtype=float)
-        MOVN = pvproperty(value=[0], dtype=float)
-        DMOV = pvproperty(value=[0], dtype=float)
-        HLS = pvproperty(value=[0], dtype=float)
-        LLS = pvproperty(value=[0], dtype=float)
-        TDIR = pvproperty(value=[0], dtype=float)
-        STOP = pvproperty(value=[0], dtype=float)
-        HOMF = pvproperty(value=[0], dtype=float)
-        HOMR = pvproperty(value=[0], dtype=float)
-        EGU = pvproperty(value=[0], dtype=float)
-        VAL = pvproperty(value=[0], dtype=float)
-        RBV = pvproperty(value=[0], dtype=float)
-        @VAL.putter
-        async def VAL(self, instance, value):
-            print(f'Setting {instance} to {value}')
-            # alsdac.MoveMotor(name, value)
+class AnalogInput(PVGroup):
+    value = pvproperty(value=[0], dtype=float, read_only=True)
 
-            # TODO: LABVIEW TCP interface has no command to get the setpoint; request this addition; fill in getter
+    @value.getter
+    async def value(self, instance):
+        return alsdac.GetFreerun(self.prefix[:-1])
 
-        # # TODO: And the readback getter
-        # @VAL.getter
-        # async def VAL(obj, instance):
-        #     # NOTE: this is effectively a no-operation method
-        #     # that is, with or without this method definition, self.readback.value
-        #     # will be returned automatically
-        #     return obj._value
 
-        @RBV.getter
-        async def RBV(self, instance):
-            return alsdac.GetMotorPos(name)
-    return Motor
+class Motor(PVGroup):
+    # FIXME: most of these don't actually do anything
+    OFF = pvproperty(value=[0], dtype=float)
+    DIR = pvproperty(value=[0], dtype=float)
+    FOFF = pvproperty(value=[0], dtype=float)
+    SET = pvproperty(value=[0], dtype=float)
+    VELO = pvproperty(value=[0], dtype=float)
+    ACCL = pvproperty(value=[0], dtype=float)
+    MOVN = pvproperty(value=[0], dtype=float)
+    DMOV = pvproperty(value=[0], dtype=float)
+    HLS = pvproperty(value=[0], dtype=float)
+    LLS = pvproperty(value=[0], dtype=float)
+    TDIR = pvproperty(value=[0], dtype=float)
+    STOP = pvproperty(value=[0], dtype=float)
+    HOMF = pvproperty(value=[0], dtype=float)
+    HOMR = pvproperty(value=[0], dtype=float)
+    EGU = pvproperty(value=[0], dtype=float)
+    VAL = pvproperty(value=[0], dtype=float)
+    RBV = pvproperty(value=[0], dtype=float)
+    @VAL.putter
+    async def VAL(self, instance, value):
+        print(f'Setting {instance} to {value}')
+        # alsdac.MoveMotor(name, value)
+
+    # TODO: LABVIEW TCP interface has no command to get the setpoint; request this addition; fill in getter
+
+    # # TODO: And the readback getter
+    # @VAL.getter
+    # async def VAL(obj, instance):
+    #     # NOTE: this is effectively a no-operation method
+    #     # that is, with or without this method definition, self.readback.value
+    #     # will be returned automatically
+    #     return obj._value
+
+    @RBV.getter
+    async def RBV(self, instance):
+        return alsdac.GetMotorPos(self.prefix[:-1])
+
 
 class Beamline(PVGroup):
 
@@ -68,19 +64,19 @@ class Beamline(PVGroup):
     class Detectors(PVGroup):
         names = alsdac.ListInstruments()
         for name in names:
-            locals()[name] = SubGroup(Instrument(name), prefix=name+':')
+            locals()[name] = SubGroup(Instrument, prefix=name+':')
 
     @SubGroup(prefix='ais:')
     class AnalogInputs(PVGroup):
         names = alsdac.ListAIs()
         for name in names:
-            locals()[name] = SubGroup(AnalogInput(name), prefix=name+'.')
+            locals()[name] = SubGroup(AnalogInput, prefix=name+'.')
 
     @SubGroup(prefix='motors:')
     class Motors(PVGroup):
         names = alsdac.ListMotors()
         for name in names:
-            locals()[name] = SubGroup(Motor(name), prefix=name+'.')
+            locals()[name] = SubGroup(Motor, prefix=name+'.')
 
 
 if __name__ == '__main__':
