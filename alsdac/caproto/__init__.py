b44e840f08dedcbacd8319bd0ea3382f30b538ff
Ronald Pandolfi <ronpandolfi@gmail.com>
2018-10-01 13:50:50 -0700

    Add image reshaping

--- alsdac/caproto/__init__.py
+++ alsdac/caproto/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from caproto.server import (pvproperty, PVGroup, SubGroup,
                             ioc_arg_parser, run, records)
+from caproto._dbr import ChannelType
 
 import alsdac
 import numpy as np
@@ -11,21 +12,48 @@ class LVGroup(PVGroup):
         return self.prefix.split(':')[-1].split('.')[0]
 
 
+
 class Instrument(LVGroup):
     trigger = pvproperty(value=[0], dtype=bool)
     read = pvproperty(value=[0], dtype=float)
+    scalarread = pvproperty(value=[0], dtype=float)
 
     # Non-standard PVs
     exposure_time = pvproperty(value=[1], dtype=float)
     exposure_counts = pvproperty(value=[1], dtype=int)
+    size_x = pvproperty(value=[0], dtype=int)
+    size_y = pvproperty(value=[0], dtype=int)
+
+    last_capture = None
 
     @trigger.putter
     async def trigger(self, instance, value):
         alsdac.StartInstrumentAcquire(self.devicename, self.exposure_time)
+        self.last_capture = None
 
     @read.getter
     async def read(self, instance):
-        return alsdac.GetInstrumentAcquired2D(self.devicename)
+        if self.last_capture is not None:
+            return self.last_capture.flatten()
+        self.last_capture = alsdac.GetInstrumentAcquired2D(self.devicename)
+        await self.size_x.write(self.last_capture.shape[0])
+        await self.size_y.write(self.last_capture.shape[1])
+        return self.last_capture.flatten()
+
+    @scalarread.getter
+    async def scalarread(self, instance):
+        if self.last_capture is not None:
+            return self.reduce_to_scalar(self.last_capture)
+        self.last_capture = alsdac.GetInstrumentAcquired2D(self.devicename)
+        await self.size_x.write(self.last_capture.shape[0])
+        await self.size_y.write(self.last_capture.shape[1])
+        scalar = self.reduce_to_scalar(self.last_capture)
+        return scalar
+
+    @staticmethod
+    def reduce_to_scalar(image):
+        shape=image.shape
+        return image[shape[0] // 2 - 2:shape[0] // 2 + 2, shape[1] // 2 - 2:shape[1] // 2 + 2].sum()
 
 
 class AnalogInput(LVGroup):  # AIFields
@@ -56,13 +84,13 @@ class Motor(LVGroup):  # MotorFields
     STOP = pvproperty(value=[0], dtype=float)
     HOMF = pvproperty(value=[0], dtype=float)
     HOMR = pvproperty(value=[0], dtype=float)
-    EGU = pvproperty(value=[0], dtype=float)
+    EGU = pvproperty(value='units', dtype=str)
     VAL = pvproperty(value=[0], dtype=float)
     RBV = pvproperty(value=[0], dtype=float)
 
     @VAL.putter
     async def VAL(self, instance, value):
-        alsdac.MoveMotor(self.devicename, value)
+        alsdac.MoveMotor(self.devicename, value[0])
 
     # TODO: LABVIEW TCP interface has no command to get the setpoint; request this addition; fill in getter
 
@@ -81,28 +109,53 @@ class Motor(LVGroup):  # MotorFields
 
 class Beamline(PVGroup):
     @SubGroup(prefix='instruments:')
-    class Detectors(PVGroup):
+    class Detectors(LVGroup):
         names = alsdac.ListInstruments()
         for name in names:
-            locals()[name] = SubGroup(Instrument, prefix=name + ':')
+            locals()[name] = SubGroup(Instrument, prefix=name + '.')
+
+        devices = pvproperty(value=[], dtype=str)
+
+        @devices.getter
+        async def devices(self, instance):
+            return list(alsdac.ListInstruments())
 
     @SubGroup(prefix='ais:')
-    class AnalogInputs(PVGroup):
+    class AnalogInputs(LVGroup):
         names = alsdac.ListAIs()
         for name in names:
             locals()[name] = SubGroup(AnalogInput, prefix=name + '.')
 
+        devices = pvproperty(value=[], dtype=str)
+
+        @devices.getter
+        async def devices(self, instance):
+            return list(alsdac.ListAIs())
+
     @SubGroup(prefix='dios:')
-    class DigitalInputOutputs(PVGroup):
+    class DigitalInputOutputs(LVGroup):
         names = alsdac.ListDIOs()
         for name in names:
             locals()[name] = SubGroup(DigitalInputOutput, prefix=name + '.')
 
+        devices = pvproperty(value=[], dtype=str)
+
+        @devices.getter
+        async def devices(self, instance):
+            return list(alsdac.ListDIOs())
+
+
     @SubGroup(prefix='motors:')
-    class Motors(PVGroup):
+    class Motors(LVGroup):
         names = alsdac.ListMotors()
         for name in names:
             locals()[name] = SubGroup(Motor, prefix=name + '.')
+
+        devices = pvproperty(value=[], dtype=str)
+
+        @devices.getter
+        async def devices(self, instance):
+            return list(alsdac.ListMotors())
 
 
 if __name__ == '__main__':
@@ -116,6 +169,6 @@ if __name__ == '__main__':
     # import os
     # os.environ['OPHYD_CONTROL_LAYER']='caproto'
     # import ophyd
-    # a = ophyd.EpicsSignal('beamline:motors:fake.RBV','beamline:m1:VAL')
-    # b = ophyd.EpicsMotor('beamline:motors:fake', name='fake')
-    # c = ophyd.
+    # x = ophyd.EpicsMotor('beamline:motors:esp300axis2', name='x')
+    # z = ophyd.EpicsMotor('beamline:motors:esp300axis2', name='z')
+    # c = ophyd.EpicsSignal('beamline:instruments:ptGreyInstrument.read', name='cam')
